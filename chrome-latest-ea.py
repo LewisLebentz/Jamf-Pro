1422181221024f771b904faafb961b6d508586b3
Lewis Lebentz <LewisLebentz@users.noreply.github.com>
2017-03-08 14:57:14 +0000

    Create chrome-latest-ea.py

---
+++ chrome-latest-ea.py
@@ -0,0 +1,32 @@
+import json
+import urllib2
+import os.path
+import plistlib
+
+url = 'http://omahaproxy.appspot.com/all.json'
+resp = urllib2.urlopen(url)
+
+
+data = json.loads(resp.read())
+
+for each in data:
+    if each.get("os") == "mac":
+        versions = each.get("versions")
+        for version in versions:
+            if version.get("channel") == "stable":
+                latest = (version.get("current_version"))
+                print latest
+
+print os.path.exists("/Applications/Google Chrome.app")
+
+plistloc = "/Applications/Google Chrome.app/Contents/Info.plist"
+
+pl = plistlib.readPlist(plistloc)
+pver = pl["CFBundleShortVersionString"]
+print pver
+
+if latest == pver:
+        print "<result>Latest</result>"
+
+else:
+        print "<result>Old</result>"
